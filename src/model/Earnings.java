953ee6d7db61373a96a82b044ebb477152c748e0
Richip4 <90368075+Richip4@users.noreply.github.com>
2021-12-12 19:45:19 -0700

    Add files via upload

--- src/model/Earnings.java
+++ src/model/Earnings.java
@@ -1,49 +1,117 @@
 package model;
 
 import static java.lang.Math.pow;
-
+/**
+ * Earnings class is used to calculate total earnings of an investment with monthly contributions
+ * 
+ * @author Richi, Josh, Jaza
+ * 
+ */
 public class Earnings extends Input{
+	//monthlyContributions instance variable is the amount that will be contributed to investment every month
 	private double monthlyContributions;
+	//compoundingPeriod instance variable is the compounding period for the interest rate, default is "Annually"
 	private String compoundingPeriod = "Annually";
 	
-	public Earnings(double annualInterestRate, double currentSavings, double monthlyContributions, String compoundingPeriod) {
-		super(annualInterestRate, currentSavings);
+	/**
+	 * Class constructor
+	 * Sets the <code>monthlyContributions</code> and <code>compoundingPeriod</code> instance variables
+	 * and calls the constructor in the parent class to set interestRate and currentSavings instance variables in the parent class
+	 * 
+	 * Precondition: <code>interestRate</code>, <code>currentSavings</code>, <code>monthlyContributions</code> must be a double, <code>compoundingPeriod</code> must be a String
+	 * Postcondition: Instance variables will be set
+	 * 
+	 * @param interestRate The interest rate to use for calculation
+	 * @param currentSavings The amount of initial savings to use for calculation
+	 * @param monthlyContributions The amount that will be added every month
+	 * @param compoundingPeriod The compounding period of the interest
+	 */
+	public Earnings(double interestRate, double currentSavings, double monthlyContributions, String compoundingPeriod) {
+		super(interestRate, currentSavings);
 		setMonthlyContributions(monthlyContributions);
 		setCompoundingPeriod(compoundingPeriod);
 	}
-
+	
+	/**
+	 * getter method for <code>monthlyContributions</code> instance variable
+	 * 
+	 * Precondition: none
+	 * Postcondition: returns <code>monthlyContributions</code> instance variable
+	 * 
+	 * @return returns the value of monthlyContributions
+	 */
 	public double getMonthlyContributions() {
 		return monthlyContributions;
 	}
-
+	
+	/**
+	 * setter method for <code>monthlyContributions</code> instance variable
+	 * 
+	 * Precondition: monthlyContributions must be of type double
+	 * Postcondition: <code>monthlyContributions</code> instance variable is set to the value of the parameter
+	 * 
+	 * @param monthlyContributions The value that the <code>monthlyContributions</code> instance variable will be set to
+	 */
 	public void setMonthlyContributions(double monthlyContributions) {
 		this.monthlyContributions = monthlyContributions;
 	}
 	
-	public int getCompoundingPeriod() {
+	/**
+	 * getter method for the integer value of the <code>compoundingPeriod</code> instance variable
+	 * 
+	 * Precondition: none
+	 * Postcondition: <code>compoundingPeriod</code> will be turned into its integer equivalent and returned
+	 * 
+	 * @return returns the integer value for the type of compounding. Monthly is 12, Quarterly is 4, Semi-Annually is 2, Annually is 1
+	 */
+	public int getCompoundingPeriodValue() {
+		//returns 12 if the compounding period is monthly
 		if(compoundingPeriod.equals("Monthly"))
 			return 12;
+		//returns 4 if the compounding period is quarterly
 		else if(compoundingPeriod.equals("Quarterly"))
 			return 4;
+		//returns 2 if the compounding period is semi-annually
 		else if(compoundingPeriod.equals("Semi-Annually"))
 			return 2;
+		//returns 1 which is the default value of annually
 		else
 			return 1;
 	}
-
+	
+	/**
+	 * setter method for the <code>compoundingPeriod</code> instance variable
+	 * 
+	 * Precondition: <code>compoundingPeriod</code> must be of type String
+	 * Postcondition: the <code>compoundingPeriod</code> instance variable will be set
+	 * 
+	 * @param compoundingPeriod the type of compounding that the <code>compoundingPeriod</code> instance variable will be set to
+	 */
 	public void setCompoundingPeriod(String compoundingPeriod) {
 		this.compoundingPeriod = compoundingPeriod;
 	}
 	
+	/**
+	 * calculateAnnualEarnings method
+	 * takes the information from all the instance variables to calculate
+	 * how much earnings will be made with those specifications
+	 * 
+	 * Precondition: All instance variables must not be null
+	 * Postcondition: calculates the earnings from the data in the instance variables and returns it
+	 * 
+	 * @return returns the earnings that will be made
+	 */
 	public double calculateAnnualEarnings() {
-		double earnings = (super.getCurrentSavings() * pow((1 + (super.getInterestRate()/getCompoundingPeriod())), super.getTimeElapsed() * getCompoundingPeriod())) + (monthlyContributions * ((12/getCompoundingPeriod())* (((pow((1 + (super.getInterestRate()/getCompoundingPeriod())), super.getTimeElapsed() * getCompoundingPeriod())) - 1) / (super.getInterestRate()/getCompoundingPeriod()))));
+		//calculate the earnings using the compound interest + monthly contributions formula
+		double earnings = (super.getCurrentSavings() * pow((1 + (super.getInterestRate()/getCompoundingPeriodValue())), super.getTimeElapsed() * getCompoundingPeriodValue())) + (monthlyContributions * ((12/getCompoundingPeriodValue())* (((pow((1 + (super.getInterestRate()/getCompoundingPeriodValue())), super.getTimeElapsed() * getCompoundingPeriodValue())) - 1) / (super.getInterestRate()/getCompoundingPeriodValue()))));
+		//rounds to 2 decimal places
 		earnings = Math.round(earnings * 100.0) / 100.0;
 		return earnings;
 	}
 	
 	//for testing only
 	public static void main(String args[]) {
-		Earnings earning = new Earnings(5, 5000, 100, "Annually");
+		Earnings earning = new Earnings(5, 5000, 100, "Quarterly");
 		earning.yearsElapsed(20, 30);
 		System.out.println(earning.calculateAnnualEarnings());
 	}
--- src/model/Input.java
+++ src/model/Input.java
@@ -1,40 +1,116 @@
 package model;
 
+/**
+ * Input class is used to get the values that are going to be used to calculate
+ * information about the investment
+ * This class is a parent to the Goals and Earnings classes
+ * 
+ * @author Richi, Jaza, Josh
+ *
+ */
 public class Input {
-	private double annualInterestRate;
+	//the interest rate used in the investment
+	private double interestRate;
+	//the amount which will be invested at the start
 	private double currentSavings;
+	//the time the investment will go for
 	int timeElapsed;
 	
+	/**
+	 * Class constructor
+	 * Sets the <code>interestRate</code> and <code>currentSavings</code> instance variables
+	 * 
+	 * Precondition: <code>interestRate</code> and <code>currentSavings</code> must be of type double
+	 * Postcondition: <code>interestRate</code> and <code>currentSavings</code> are set
+	 * 
+	 * @param annualInterestRate
+	 * @param currentSavings
+	 */
 	public Input(double annualInterestRate, double currentSavings) {
 		setInterestRate(annualInterestRate);
 		setCurrentSavings(currentSavings);
 	}
 	
+	/**
+	 * getter method for the <code>interestRate</code> instance variable
+	 * 
+	 * Precondition: none
+	 * Postcondition: returns the <code>interestRate</code> instance variable
+	 * 
+	 * @return returns the value of the <code>interestRate</code> instance variable
+	 */
 	public double getInterestRate() {
-		return annualInterestRate;
+		return interestRate;
 	}
 	
+	/**
+	 * setter method for the <code>interestRate</code> instance variable
+	 * takes the value of <code>interestRate</code> and divides it by 100
+	 * 
+	 * Precondition: <code>interestRate</code> must be of type double
+	 * Postcondition: the <code>interestRate</code> instance variable will be
+	 * set to the parameter percent in decimal form
+	 * 
+	 * @param interestRate the interest rate to be used for calculation
+	 */
 	public void setInterestRate(double interestRate) {
-		this.annualInterestRate = interestRate / 100;
+		this.interestRate = interestRate / 100;
 	}
 	
+	/**
+	 * getter method for the <code>currentSavings</code> instance variable
+	 * 
+	 * Precondition: none
+	 * Postcondition: returns the <code>currentSavings</code> instance variable
+	 * 
+	 * @return returns the value of the <code>currentSavings</code> instance variable
+	 */
 	public double getCurrentSavings() {
 		return currentSavings;
 	}
 	
+	/**
+	 * setter method for the <code>currentSavings</code> instance variable
+	 * 
+	 * Precondition: <code>currentSavings</code> must be of type double
+	 * Postcondition: the <code>currentSavings</code> instance variable
+	 * will be set to the value of the parameter
+	 * 
+	 * @param currentSavings the initial savings amount that will be invested.
+	 * the <code>currentSavings</code> instance variable will be set to this value
+	 */
 	public void setCurrentSavings(double currentSavings) {
 		this.currentSavings = currentSavings;
 	}
 	
+	/**
+	 * getter method for the <code>timeElapsed</code> instance variable
+	 * 
+	 * Precondition: none
+	 * Postcondition: returns the <code>timeElapsed</code> instance variable
+	 * 
+	 * @return returns the value of the <code>timeElapsed</code> instance variable
+	 */
 	public int getTimeElapsed() {
 		return timeElapsed;
 	}
-
-	public void yearsElapsed(int currentAge, int retirementAge) {
-		int time = retirementAge - currentAge;
+	
+	/**
+	 * yearsElapsed method takes the current age of the individual and subtracts
+	 * it from the age they plan to retire and cash out the investment at, to get
+	 * the total time the investment will go on for
+	 * 
+	 * Precondition: <code>currentAge</code> and <code>investmentEndAge</code> must be integers
+	 * Postcondition: the <code>timeElapsed</code> instance variable will be set to the value
+	 * of the <code>investmentEndAge</code> parameter minus the <code>currentAge</code> parameter
+	 * 
+	 * @param currentAge the age of the user at the moment of investing
+	 * @param investmentEndAge the age when the user plans to retire or cash out the investment
+	 */
+	public void yearsElapsed(int currentAge, int investmentEndAge) {
+		int time = investmentEndAge - currentAge;
 		timeElapsed = time;
 	}
-	
 }
 
 
