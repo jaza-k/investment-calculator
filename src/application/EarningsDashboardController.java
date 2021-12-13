93a35584827682ada90cba3b99017c91385a6887
Richip4 <90368075+Richip4@users.noreply.github.com>
2021-12-13 04:51:55 -0700

    Add files via upload

--- src/application/EarningsDashboardController.java
+++ src/application/EarningsDashboardController.java
@@ -5,6 +5,9 @@ import javafx.fxml.FXML;
 import javafx.scene.chart.CategoryAxis;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.NumberAxis;
+import javafx.scene.chart.XYChart;
+import javafx.scene.chart.XYChart.Data;
+import javafx.scene.chart.XYChart.Series;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import model.Creator;
@@ -46,7 +49,15 @@ public class EarningsDashboardController {
     void initialize() {
     	userName.setText(Creator.getInstance().getName());
     	goalAge.setText("" + Creator.getInstance().getEndingAge());
-    	amountSaved.setText("" + Creator.getInstance().createGoals());
+    	amountSaved.setText("" + Creator.getInstance().createEarnings());
+    	chartHelper();
+    }
+    
+    private void chartHelper() {
+    	XYChart.Series<Number, Number> data = new Series<Number, Number>();
+    	for (int i = 0; i <= Creator.getInstance().getEndingAge() - Creator.getInstance().getCurrentAge(); i+=2) {
+    		data.getData().add(new Data<Number, Number>(i, Creator.getInstance().specificYearEarnings(i)));
+    	}
+    	this.lineChart.getData().add(data);
     }
 }
-
