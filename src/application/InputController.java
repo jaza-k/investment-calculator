e1f43728cb890c221b5208266ff51e2886869619
Joshua <89869008+LeJoshua@users.noreply.github.com>
2021-12-12 19:49:02 -0700

    Add files via upload

--- src/application/InputController.java
+++ src/application/InputController.java
@@ -1,11 +1,39 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class InputController {
 
     @FXML
+    private Button GoalButton;
+
+    @FXML
+    private TextField annualInterestTextfield;
+
+    @FXML
+    private TextField currentSavingsTextfield;
+
+    @FXML
+    private Button earningsButton;
+
+    @FXML
     private Label header;
+
+    @FXML
+    private TextField retirementCashOutAgeTextfield;
+
+    @FXML
+    void loadEarningsView(ActionEvent event) {
+
+    }
+
+    @FXML
+    void loadGoalView(ActionEvent event) {
+
+    }
 
 }
